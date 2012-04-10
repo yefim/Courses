01b6afec35e91d549d4f7c15bd61fb3a676c18a2
Geoffrey Vedernikoff <yefim323@gmail.com>
2012-04-10 14:06:38 -0400

    added config file

--- mysql.php
+++ mysql.php
@@ -1,6 +1,7 @@
 <?php
-  $con = mysql_connect("localhost","root","");
+  include('config.php');
+  $con = mysql_connect(DB_HOST,DB_USER,DB_PASS);
   if (!$con)
     die('Could not connect: ' + mysql_error());
-  mysql_select_db('kingscourses',$con);
+  mysql_select_db(DB_DATABASE,$con);
 ?>
